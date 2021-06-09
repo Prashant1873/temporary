b31cb2ead6e0b55ba2b8ffcd862419033bf138d8
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-06-10 00:31:49 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/cArN4gEisDeD/local_manifest.git --depth 1 -b tiare 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+source build/envsetup.sh 
 lunch lineage_tiare-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
